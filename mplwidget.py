7cecbd32cd027f196681e72e20a43127c8885fb6
JungHwan <59270365+junghwanchoi@users.noreply.github.com>
2022-04-25 16:03:03 +0900

    한글 구현

--- mplwidget.py
+++ mplwidget.py
@@ -6,7 +6,7 @@ from PyQt5.QtWidgets import *
 from matplotlib.backends.backend_qt5agg import FigureCanvas
 
 from matplotlib.figure import Figure
-
+import matplotlib
     
 class MplWidget(QWidget):
     
@@ -22,4 +22,9 @@ class MplWidget(QWidget):
         self.canvas.axes = self.canvas.figure.add_subplot(111)
         self.canvas.axes_2 = self.canvas.axes.twinx()
         self.setLayout(vertical_layout)
+        # 한글 구현
+        font = {
+            'family': 'New Gulim'
+        }
+        matplotlib.rc( 'font', **font )
 
